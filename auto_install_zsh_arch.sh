d2d40059f92f377ce0633f1dff1c834cf378f0e2
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-06-01 16:52:08 +0200

    [FIX] bug wrong insert line in file

--- auto_install_zsh_arch.sh
+++ auto_install_zsh_arch.sh
@@ -1,41 +1,106 @@
 #!/bin/bash
 
+echo
+echo "-----------"
 echo "Install zsh"
+echo "-----------"
+echo
 sudo pacman -Syyu zsh -y
 
+echo
+echo "-------------------------------"
+echo "<C>D after seeing the zsh promt"
+echo "-------------------------------"
+echo
 zsh < 1
 
+echo
+echo "-----------------"
 echo "Install oh-my-zsh"
+echo "-----------------"
+echo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 
-echo "Install zsh-autosuggestions"
+echo
+echo "----------------------------"
+echo "Go to the zsh plugins folder"
+echo "----------------------------"
+echo
 cd $ZSH_CUSTOM/plugins
+
+echo
+echo "---------------------------"
 echo "Install zsh-autosuggestions"
+echo "---------------------------"
+echo
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+echo
+echo "-------------------------------"
 echo "Install zsh-syntax-highlighting"
+echo "-------------------------------"
+echo
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo
+echo "------------------------"
 echo "Install ttf-font-awesome"
+echo "------------------------"
+echo
 sudo pacman -Syyu ttf-font-awesome -y
 
+echo
+echo "---------------------"
 echo "Install powerlevel10k"
+echo "---------------------"
+echo
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cd ~/
+echo
+echo "-----------"
+echo "open .zshrc"
+echo "-----------"
+echo
+cd ~
 file=".zshrc"
-plugin_to_insert=" zsh-autosuggestions zsh-syntax-highlighting"
-theme_to_insert="powerlevel10k/powerlevel10k"
 
-
-# Read the file content into an array
+echo
+echo "-----------------------------------"
+echo "Read the file content into an array"
+echo "-----------------------------------"
+echo
 mapfile -t lines < "$file"
 
-# Insert the string at the specified line and column
-line="${lines[80 - 1]}"
-line="${line:0:13}${plugin_to_insert}${line:13}"
-lines[80 - 1]="$line"
+echo
+echo "------------------------"
+echo "Insert the plugin string"
+echo "------------------------"
+echo
+plugin_to_insert=" zsh-autosuggestions zsh-syntax-highlighting"
+line="${lines[73 - 1]}"
+line="${line:0:12}${plugin_to_insert}${line:12}"
+lines[73 - 1]="$line"
 
-line="${lines[18 - 1]}"
-line="${line:0:12}${theme_to_insert}${line:12}"
-lines[18 - 1]="$line"
+echo
+echo "-----------------------"
+echo "Insert the theme string"
+echo "-----------------------"
+echo
+theme_to_insert="powerlevel10k/powerlevel10k"
+line="${lines[11 - 1]}"
+line="${line:0:11}${theme_to_insert}${line:23}"
+lines[11 - 1]="$line"
+
+echo
+echo "-------------------------------------------"
+echo "Write the modified content back to the file"
+echo "-------------------------------------------"
+echo
+printf "%s\n" "${lines[@]}" > "$file"
+
+echo
+echo "-------------------------------------"
+echo "Shitch the default shell to ZSH shell"
+echo "-------------------------------------"
+echo
+chsh -s /bin/zsh
